ae2d451a37564e9bbb55f9b9e2e0719b42892738
Alicia Sykes <alicia@omg.lol>
2022-03-20 21:08:15 +0000

    🖱 Change cursor, depending on insert or nomal mode

---
+++ zsh/lib/cursor.zsh
@@ -0,0 +1,24 @@
+
+# See https://ttssh2.osdn.jp/manual/4/en/usage/tips/vim.html for cursor shapes
+
+cursor_block='\e[2 q'
+cursor_beam='\e[6 q'
+
+function zle-keymap-select {
+    if [[ ${KEYMAP} == vicmd ]] ||
+        [[ $1 = 'block' ]]; then
+        echo -ne $cursor_block
+    elif [[ ${KEYMAP} == main ]] ||
+        [[ ${KEYMAP} == viins ]] ||
+        [[ ${KEYMAP} = '' ]] ||
+        [[ $1 = 'beam' ]]; then
+        echo -ne $cursor_beam
+    fi
+}
+
+zle-line-init() {
+    echo -ne $cursor_beam
+}
+
+zle -N zle-keymap-select
+zle -N zle-line-init
