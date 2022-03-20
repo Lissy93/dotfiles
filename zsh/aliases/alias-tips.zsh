e2fc1690c87fdf98315baa3aa0f43029f4e7a5bb
Alicia Sykes <alicia@omg.lol>
2022-03-20 21:10:03 +0000

    🤓 Adds hook to display alias tips for long commands

---
+++ zsh/aliases/alias-tips.zsh
@@ -0,0 +1,14 @@
+
+# If an alias for the command just run exists, then show tip
+preexec_alias-finder() {
+  tip=$(alias | grep -E "=$1$")
+  if [ ! -z "$tip" ]; then
+    echo -e "\033[0;90m\e[3mTip: \e[4m$tip\033[0m"
+  fi
+}
+
+# Load add-zsh-hook
+autoload -U add-zsh-hook
+
+# Call function after command
+add-zsh-hook preexec preexec_alias-finder
