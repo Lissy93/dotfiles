ac0b6005b5416cede8f961ae0b034dbc299e2eb1
Alicia Sykes <alicia@omg.lol>
2022-08-30 17:40:38 +0100

    Adds echo intro to lets-go script

--- lets-go.sh
+++ lets-go.sh
@@ -15,8 +15,16 @@
 DOTFILES_DIR="${DOTFILES_DIR:-$HOME/Documents/config/dotfiles}"
 DOTFILES_REPO="${DOTFILES_REPO:-https://github.com/lissy93/dotfiles.git}"
 
+# Print starting message
+echo -e "\033[1;35m""Lissy93/Dotfiles Installation Script 🧰
+\033[0;35mThis script will install or update dotfiles from \033[4;35m\
+${DOTFILES_REPO}\033[0;35m into \033[4;35m${DOTFILES_DIR}\033[0;35m\n\
+Be sure that you have read and understood config which will be applied.
+If you don't want to proceed, press Ctrl+C to cancel now...\033[0m\n"
+
 # If dotfiles not yet present then clone
 if [[ ! -d "$DOTFILES_DIR" ]]; then
+  mkdir -p "${DOTFILES_DIR}" && \
   git clone --recursive ${DOTFILES_REPO} ${DOTFILES_DIR}
 fi
 
