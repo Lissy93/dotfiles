3c8f6ff5c23fd17f9639209ecbcaea2ffdc33ed8
Alicia Sykes <alicia@omg.lol>
2022-10-02 21:47:53 +0100

    Adds no clear, and updated text to lets-go

--- lets-go.sh
+++ lets-go.sh
@@ -2,8 +2,7 @@
 
 ######################################################################
 # Lissy93/dotfiles - Remote Runnable Dotfile Setup and Update Script #
-# Licensed under MIT (C) Alicia Sykes 2022 <https://aliciasykes.com> #
-#                                                                    #
+######################################################################
 # This script will clone + install, or update dotfiles from git      #
 # Be sure to read through the repo before running anything here      #
 # For more info, read docs: https://github.com/Lissy93/dotfiles      #
@@ -12,6 +11,8 @@
 # - DOTFILES_REPO - Optionally sets the source repo to be cloned     #
 # - DOTFILES_DIR - Optionally sets the local destination directory   #
 ######################################################################
+# Licensed under MIT (C) Alicia Sykes 2022 <https://aliciasykes.com> #
+######################################################################
 
 # If not already set, specify dotfiles destination directory and source repo
 DOTFILES_DIR="${DOTFILES_DIR:-$HOME/Documents/config/dotfiles}"
@@ -19,10 +20,10 @@ DOTFILES_REPO="${DOTFILES_REPO:-https://github.com/lissy93/dotfiles.git}"
 
 # Print starting message
 echo -e "\033[1;35m""Lissy93/Dotfiles Installation Script 🧰
-\033[0;35mThis script will install or update dotfiles from \033[4;35m\
-${DOTFILES_REPO}\033[0;35m into \033[4;35m${DOTFILES_DIR}\033[0;35m\n\
-Be sure that you have read and understood config which will be applied.
-If you don't want to proceed, press Ctrl+C to cancel now...\033[0m\n"
+\033[0;35mThis script will install or update specified dotfiles:
+- From \033[4;35m${DOTFILES_REPO}\033[0;35m
+- Into \033[4;35m${DOTFILES_DIR}\033[0;35m
+Be sure you've read and understood the what will be applied.\033[0m\n"
 
 # If dotfiles not yet present then clone
 if [[ ! -d "$DOTFILES_DIR" ]]; then
@@ -33,6 +34,6 @@ fi
 # Execute setup or update script
 cd "${DOTFILES_DIR}" && \
 chmod +x ./install.sh && \
-./install.sh
+./install.sh --no-clear
 
 # EOF
