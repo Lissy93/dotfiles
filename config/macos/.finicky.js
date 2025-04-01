6148ff0fa232241489f27ea00746bb173ef8a0e3
Alicia Sykes <alicia@omg.lol>
2025-04-01 19:54:36 +0100

    Adds more links to finicky

--- config/macos/.finicky.js
+++ config/macos/.finicky.js
@@ -1,4 +1,4 @@
-// Config file for Finicky
+  // Config file for Finicky
 // Specifies which browser to open based on the URL
 // Docs: https://github.com/johnste/finicky
 module.exports = {
@@ -12,27 +12,46 @@ module.exports = {
   handlers: [
     { // Open work non-coding stuff in Chromium
       // browser: "/Applications/Chromium.app",
-      // We must use Chrome, because IT is evil.
-      browser: "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome",
+      // We must now use Chrome, because IT is evil.
+      browser: "/Applications/Google\ Chrome.app",
       match: [
+        // Generic work things
         "*.google.com/*",
-        "*and-digital.okta.com/*",
-        "*and-digital.zoom.us/*",
         "*atlassian.com/*",
         "*dev.azure.com/*",
         "*docs.google.com/*",
-        "*luna.and-digital.com/*",
         "*miro.com/*",
-        "*zoom.com/*",
-        "*.atlassian.net/*"
+        "*.zoom.com/*",
+        "*.atlassian.net/*",
+        "*.pagerduty.com/*",
+        "*.sentry.io/*",
+        "app.incident.io/*",
+        "app.clickup.com/*",
+        "console.cloud.google.com/*",
+        "*.myfreshworks.com/*",
+        "*.openvpn.com/*",
+        "*.slack.com/*",
+        "*getmontecarlo.com/*",
+
+        // Loveholidays stuff
+        "*.lvh.systems/*",
+        "*.loveholidays.com/*",        
+        "*.hibob.com/*",
+        "github.com/loveholidays/*",
+        
+
+        // I love Firefox, but Chrome devtools is better
+        "localhost:*",
+
+        // AND Digital stuff
+        "*and-digital.okta.com/*",
+        "*and-digital.zoom.us/*",
+        "*luna.and-digital.com/*",
+        
       ],
     },
     { // Open coding stuff in Firefox
       browser: "/Applications/Firefox.app",
-      match: [],
-    },
-    { // Open personal stuff in Orion
-      browser: "/Applications/Orion.app",
       match: [],
     },
   ]
